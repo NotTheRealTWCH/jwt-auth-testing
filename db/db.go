831e8f0cfd4b295059f035bb1fab63edff06b1db
Twitch <ianletwitch@gmail.com>
2018-01-03 02:43:40 +1100

    Update db.go

--- db/db.go
+++ db/db.go
@@ -135,6 +135,8 @@ func generateBcryptHash(password string) (string, error) {
 }
 
 func checkPasswordAgainstHash(hash string, password string) error {
+	fmt.Println("Test: ")
 	fmt.Println(bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)))
+	fmt.Println(" - ")
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
